ef34e9312f9fccec278c080e5f40e37f645064a4
dreadPir8Rbrts <dpr@dreadsvault.com>
2026-04-23 13:08:58 -0400

    fix: add DO app URL to CORS allowlist

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://leftovers-gg-isud9.ondigitalocean.app",
         "https://leftovers.gg",
         "https://www.leftovers.gg",
     ],
